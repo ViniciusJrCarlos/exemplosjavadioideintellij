5e69d4f1f34d2cf5f6ff028135de0399376566ca
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2023-02-20 11:47:09 -0300

    primeiro commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,21 @@
+package br.com.dio;
+
+//import br.com.dio.model.Gato;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        /*int a  = 5;
+        int b = 6;
+        System.out.println("hello World..." + (a+b));*/
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+//        System.out.println(gato);
+    }
+
+}
